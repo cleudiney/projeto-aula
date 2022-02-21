85d1526593897fbdcb4a8a203cfeaf6256eeee8a
cleudiney <cleudiney@hotmail.com>
2022-02-21 14:59:33 -0300

    declaração das constantes para medição de distancia Definições dos GPIOs para leitura do sensor ultrasonico Definições de operação do sensor ultrasônico configuração do setup  configuração do loop

--- irene/irene.ino
+++ irene/irene.ino
@@ -1,3 +1,8 @@
+//declaração das bibliotecas
+#include <Ultrasonic.h>
+
+
+
 //declaração das funções
 
 void motomove();
@@ -6,6 +11,10 @@ void sensores();
 void calc_distancia();
 void olha_lados();
 void decide_andar();
+int ler_distancia(void);
+void mover_frente(void);
+void mover_tras(void);
+void parar(void);
 
 
 //declaração dos sensores 
@@ -31,26 +40,77 @@ int bot2 = 3;
 
 
 
+//declaração  das constantes
 
-const int motorPin = 2;
-const int buttonPin = 7;
-int buttonState = 0;
+const int PIN_MOTOR_IN1 = 2;
+const int PIN_MOTOR_IN2 = 3;
+const int PIN_MOTOR_IN3 = 4;
+const int PIN_MOTOR_IN4 = 5;
+
+
+/* Definições dos portas para leitura do sensor ultrasonico */
+const int PINO_SENSOR_ECHO = 13;
+const int PINO_SENSOR_TRIGGER = 12;
+
+/* Definições de operação do sensor ultrasônico */
+// declaracao das constantes auxiliares para controlar os motores
+const int DISTANCIA_SEGURA = 25; // [cm]
+const int PAUSA = 100; // [ms]
+
+
 
 void setup() 
 {
-  pinMode(buttonPin, INPUT);
-  pinMode(motorPin, OUTPUT);
+ // configura os pinos do sensor ultrassonico
+  pinMode(PINO_SENSOR_ECHO, INPUT); // entrada
+  pinMode(PINO_SENSOR_TRIGGER, OUTPUT); // saida
+  digitalWrite(PINO_SENSOR_TRIGGER, LOW); // por padrao em nivel baixo (sem sinal)
+
+  // configura os pinos do driver de motor
+  pinMode(PIN_MOTOR_IN1, OUTPUT); // saida
+  pinMode(PIN_MOTOR_IN2, OUTPUT); // saida
+  pinMode(PIN_MOTOR_IN3, OUTPUT); // saida
+  pinMode(PIN_MOTOR_IN4, OUTPUT); // saida
+  parar(); // robo parado por padrao
 }
 
-void loop() 
-{
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) 
-  {
+void loop() {
+  // le a distancia
+  int distancia = ler_distancia();
 
-  }
-  else 
-  {
+  // verifica se ha um obstaculo na frente
+  if(distancia < DISTANCIA_SEGURA){
+    // para o robo
+    parar();
+    delay(500); // pausa para o proximo movimento
 
+    // move o robo para tras
+    mover_tras();
+
+    // para o robo apos um curto intervalo
+    delay(1000);
+    parar();
+
+    // escolhe um dos lados para girar
+    bool par = (millis() % 2 == 0) ? true : false;
+    if(par){
+      // gira o motor 1 para a frente
+      digitalWrite(PIN_MOTOR_IN1, HIGH);
+      digitalWrite(PIN_MOTOR_IN2, LOW);
+    } else {
+      // gira o motor 2 para a frente
+      digitalWrite(PIN_MOTOR_IN3, HIGH);
+      digitalWrite(PIN_MOTOR_IN4, LOW);
+    }
+
+    // para o robo apos um curto intervalo
+    delay(500);
+    parar();
+  } else { // senao
+    // move o robo para a frente
+    mover_frente();
   }
+
+  // pausa para a proxima leitura
+  delay(PAUSA);
 }
--- irene/olha_lados.ino
+++ irene/olha_lados.ino
@@ -3,5 +3,5 @@ void olha_lados()
 	
 
 
-	
+		
 }
